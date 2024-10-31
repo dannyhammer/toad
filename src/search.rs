baf21c401076619704777ee007cf612f39d00bfa
Danny Hammer <HammerAPI@gmail.com>
2024-10-31 15:00:47 -0600

    refactor: removed conditional in score_move to allow quicker MVV-LVA table access
    bench: 13225972

--- src/search.rs
+++ src/search.rs
@@ -13,7 +13,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-use chessie::{Game, Move, MoveList, PieceKind, Position, ZobristKey};
+use chessie::{Color, Game, Move, MoveList, Piece, PieceKind, Position, ZobristKey};
 use uci_parser::{UciInfo, UciResponse, UciSearchOptions};
 
 use crate::{tune, value_of, Evaluator, Score, TTable, TTableEntry};
@@ -273,7 +273,7 @@ impl<'a> Search<'a> {
 
     /// Start the search on the supplied [`Game`], returning a [`SearchResult`].
     ///
-    /// This is the entrypoint of the search, and prints UCI info before calling [`Self::iterative_deepening`],
+    /// This is the entrypoint of the search, and prints UCI info before starting iterative deepening.
     /// and concluding by sending the `bestmove` message and exiting.
     #[inline(always)]
     pub fn start<const DEBUG: bool>(mut self, game: &Game) -> SearchResult {
@@ -750,9 +750,7 @@ fn score_move(game: &Game, mv: &Move, tt_move: Option<Move>) -> Score {
     // Capturing a high-value piece with a low-value piece is a good idea
     // if let Some(victim) = game.kind_at(mv.to()) {
     if let Some(victim) = game.piece_at(mv.to()) {
-        if victim.color() != piece.color() {
-            score += MVV_LVA[piece.kind()][victim];
-        }
+        score += MVV_LVA[piece][victim];
     }
 
     -score // We're sorting, so a lower number is better
@@ -774,18 +772,31 @@ fn score_move(game: &Game, mv: &Move, tt_move: Option<Move>) -> Score {
 /// E   Q| 100   2300  2400  4100  8100  0     
 /// R   K| 1000  3200  3300  5000  9000  0     
 /// ```
-const MVV_LVA: [[i32; PieceKind::COUNT]; PieceKind::COUNT] = {
-    let mut matrix = [[0; PieceKind::COUNT]; PieceKind::COUNT];
-    let count = PieceKind::COUNT;
+///
+/// Note that the actual table is different, as it has size `12x12` instead of `6x6`
+/// to account for the fact that castling is denoted as `KxR`.
+/// The values are still the same, though, just without allowing captures of friendly pieces.
+///
+/// See [`print_mvv_lva_table`] to display this table.
+const MVV_LVA: [[i32; Piece::COUNT]; Piece::COUNT] = {
+    let mut matrix = [[0; Piece::COUNT]; Piece::COUNT];
+    let count = Piece::COUNT;
 
     let mut attacker = 0;
     while attacker < count {
         let mut victim = 0;
+        let atk_color = Color::from_bool(attacker < PieceKind::COUNT);
 
-        // The -1 here is to remove scores for capturing the King
-        while victim < count - 1 {
-            let atk = PieceKind::from_bits_unchecked(attacker as u8);
-            let vtm = PieceKind::from_bits_unchecked(victim as u8);
+        while victim < count {
+            let atk = PieceKind::from_bits_unchecked(attacker as u8 % 6);
+            let vtm = PieceKind::from_bits_unchecked(victim as u8 % 6);
+
+            let vtm_color = Color::from_bool(victim < PieceKind::COUNT);
+
+            // Remove scores for capturing the King and friendly pieces (KxR for castling)
+            let can_capture = (atk_color.index() != vtm_color.index()) // Different colors
+                && victim != count - 1 // Can't capture White King
+                && victim != PieceKind::COUNT - 1; // Can't capture Black King
 
             // Rustic's way of doing things; Arbitrary increasing numbers for capturing pairs
             // bench: 27609398 nodes 5716479 nps
@@ -805,7 +816,7 @@ const MVV_LVA: [[i32; PieceKind::COUNT]; PieceKind::COUNT] = {
             //     10 * value_of(vtm) - value_of(atk)
             // };
 
-            matrix[attacker][victim] = score;
+            matrix[attacker][victim] = score * can_capture as i32;
             victim += 1;
         }
         attacker += 1;
@@ -816,22 +827,18 @@ const MVV_LVA: [[i32; PieceKind::COUNT]; PieceKind::COUNT] = {
 /// Utility function to print the MVV-LVA table
 #[allow(dead_code)]
 pub fn print_mvv_lva_table() {
-    print!("\nX   ");
-    for victim in PieceKind::iter() {
-        let v = victim.char().to_ascii_uppercase();
-
-        print!("{v:<6}");
+    print!("\nX  ");
+    for victim in Piece::iter() {
+        print!("{victim}     ");
     }
     print!("\n +");
-    for _ in PieceKind::iter() {
+    for _ in Piece::iter() {
         print!("------");
     }
     println!("-+");
-    for attacker in PieceKind::iter() {
-        let a = attacker.char().to_ascii_uppercase();
-
-        print!("{a}| ");
-        for victim in PieceKind::iter() {
+    for attacker in Piece::iter() {
+        print!("{attacker}| ");
+        for victim in Piece::iter() {
             let score = MVV_LVA[attacker][victim];
             print!("{score:<4}  ")
         }
