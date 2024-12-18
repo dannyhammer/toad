01d70a98301d39efcfdf18df5c4ce4ad50485f2f
Danny Hammer <HammerAPI@gmail.com>
2024-12-18 08:59:07 -0700

    chore: LMP depth-based formula coefficient 8 -> 6
    bench: 4105174

--- toad/src/search.rs
+++ toad/src/search.rs
@@ -854,7 +854,7 @@ impl<'a, Log: LogLevel, V: Variant> Search<'a, Log, V> {
             // Move pruning techniques
             if !Node::PV && !new.is_in_check() && !best.mated() {
                 // late move pruning
-                let min_lmp_moves = 8 * depth.plies() as usize;
+                let min_lmp_moves = 6 * depth.plies() as usize;
                 if i >= min_lmp_moves {
                     break;
                 }
