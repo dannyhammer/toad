26199b0bc0b600e80e04073326dfb4195c74ceef
Danny Hammer <HammerAPI@gmail.com>
2025-01-07 07:02:44 -0700

    refactor: max history bonus is 2^13-1
    bench: 8612666

--- src/tune.rs
+++ src/tune.rs
@@ -63,7 +63,7 @@ pub(crate) use min_aspiration_window_depth;
 /// Maximum bonus to apply to a move via History Heuristic.
 macro_rules! max_history_bonus {
     () => {
-        16_384
+        8_192
     };
 }
 pub(crate) use max_history_bonus;
